f1222e18fc76a003260b80eaec278acd184ce0ae
aabhura <abdullah.abhura@ada.ac.uk>
2022-11-04 12:27:00 +0000

    difference from answer function

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ import random
 num = 0
 def generate_random_number()
   num = random.randint(0,10000)
+
+def difference_from_answer(guess, answer)
+  if guess = answer:
+    return "Correct"
+  elif guess > answer:
+    return "Too high"
+  else:
+    return "Too low"
   
